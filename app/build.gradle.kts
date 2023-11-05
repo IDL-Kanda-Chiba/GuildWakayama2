ad858691b004ef311b2b3babbfd5a24f6a44bdd8
M-Chiba <109466555+M-Chiba11@users.noreply.github.com>
2023-11-05 21:50:17 +0900

    2023-11-05

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.GuildWakayama2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.GuildWakayama2"
