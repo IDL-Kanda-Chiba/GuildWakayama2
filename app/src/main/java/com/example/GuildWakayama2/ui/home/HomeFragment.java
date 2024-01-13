d418fbf36a8b8c76f82cf16158885ec45a2fcd45
M-Chiba <109466555+M-Chiba11@users.noreply.github.com>
2024-01-13 13:13:40 +0900

    未使用のimportを削除
    未使用のimportを削除しました。

--- app/src/main/java/com/example/GuildWakayama2/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/GuildWakayama2/ui/home/HomeFragment.java
@@ -1,32 +1,33 @@
 package com.example.GuildWakayama2.ui.home;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
 import com.example.GuildWakayama2.databinding.FragmentHomeBinding;
+import com.example.GuildWakayama2.ui.LocationManagerHelper;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import android.widget.ImageView;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.example.GuildWakayama2.ui.LocationManagerHelper;
-import android.content.Intent;
-import android.net.Uri;
+
 import java.util.Locale;
-
-
 
 
 public class HomeFragment extends Fragment {
@@ -51,6 +52,16 @@ public class HomeFragment extends Fragment {
     private void setupEventListener() {
         // イベントリスナーを設定
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String user_name;
+        /*
+        if(Objects.requireNonNull(user).getDisplayName() != null){
+            user_name = user.getDisplayName();
+        }
+        else {
+            user_name = "DefaultName";
+        }
+         */
+        //user_name = user.getDisplayName(); // nullに対する処理を書いておく
         String userId = user.getUid();
 
         DatabaseReference eventReference = FirebaseDatabase.getInstance().getReference("events").child("user-id").child(userId);
